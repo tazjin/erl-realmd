53bc63f0562d3aaebe4960b7b0588537139dd297
Vincent Ambo <vincent@kivra.com>
2015-03-21 00:41:46 +0100

    Rudimentary first packet parsing

---
+++ src/realmd_defines.hrl
@@ -0,0 +1,11 @@
+%% Login error codes
+-define(ce_success, 0).
+-define(ce_ipban, 1).
+-define(ce_account_closed, 3).
+-define(ce_no_account, 4).
+-define(ce_account_in_use, 6).
+-define(ce_preorder_time_limit, 7).
+-define(ce_server_full, 8).
+-define(ce_wrong_build_number, 9).
+-define(ce_update_client, 10).
+-define(ce_account_freezed, 12).
--- src/realmd_protocol.erl
+++ src/realmd_protocol.erl
@@ -1,6 +1,8 @@
 -module(realmd_protocol).
 -behaviour(ranch_protocol).
 
+-include("realmd_defines.hrl").
+
 -export([start_link/4]).
 -export([init/4]).
 
@@ -13,18 +15,44 @@ init(Ref, Socket, Transport, _Opts = []) ->
     loop(Socket, Transport).
 
 loop(Socket, Transport) ->
-    case Transport:recv(Socket, 0, 5000) of
-        {ok, <<0,Data/binary>>} ->
-            lager:debug("Received logon challenge, dispatching to handler"),
-            handle_auth_logon_challenge(Socket, Transport, Data),
+    case Transport:recv(Socket, 1, 5000) of
+        {ok, <<0>>} ->
+            lager:debug("Dispatching client logon challenge handler"),
+            handle_auth_logon_challenge(Socket, Transport),
             loop(Socket, Transport);
-%%            Transport:send(Socket, Data),
         _ ->
             lager:debug("closing socket"),
             ok = Transport:close(Socket)
     end.
 
+handle_auth_packet() -> ok.
+
 %% Protocol handlers
-handle_auth_logon_challenge(Socket, Transport, Data) ->
-    lager:info("echoing ~p", [Data]),
-    Transport:send(Socket, Data).
+
+%%%%%%%%%%%%%%%%%%%%
+%% Auth challenge %%
+%%%%%%%%%%%%%%%%%%%%
+
+handle_auth_logon_challenge(Socket, Transport) ->
+    case Transport:recv(Socket, 7, 1000) of
+        {ok, <<_Err, Len:16/little-integer, "WoW\0">>} ->
+            lager:debug("Received auth challenge, length ~B", [Len]),
+            {ok, Data} = Transport:recv(Socket, Len - 4, 1000),
+            dispatch_auth_challenge(Socket, Transport, Data);
+        _ ->
+            lager:debug("Received invalid auth challenge"),
+            ok = Transport:close(Socket)
+    end.
+
+dispatch_auth_challenge(Socket, Transport, Data) ->
+    <<1, 12, 1, Build:16/little-integer,
+      Platform:3/binary, "\0",
+      OS:3/binary, "\0",
+      Country:4/binary,
+      _Timezone:32/integer,
+      IP:4/binary,
+      ILen:8/integer,
+      I:ILen/binary,
+      _Rest/binary>> = Data,
+    LogonReply = <<0, 0, 0, 0>>
+    lager:debug("ok ~p", [[Build, Platform, OS, Country, IP, ILen, I]]).
