7b6e9531abf174e95d8b6f3d508292051797420b
Vincent Ambo <vincent@kivra.com>
2015-03-22 19:59:40 +0100

    Nothing works but I'll try again later
    http://web.archive.org/web/20130513005036/http://skullsecurity.org/wiki/index.php/SRP

--- src/realmd_auth_proof.erl
+++ src/realmd_auth_proof.erl
@@ -8,15 +8,26 @@ hexstring(Binary) when is_binary(Binary) ->
         fun(X) -> io_lib:format("~2.16.0b", [X]) end, 
         binary_to_list(Binary))).
 
+
+%% typedef struct AUTH_LOGON_PROOF_S_BUILD_6005
+%% {
+%%   uint8   cmd;
+%%   uint8   error;
+%%   uint8   M2[20];
+%%   // uint32  unk1;
+%%   uint32  unk2;
+%%   // uint16  unk3;
+%% } sAuthLogonProof_S_BUILD_6005;
+
 handle_auth_proof(Socket, Transport, S) ->
     lager:debug("entered await_auth_proof"),
     case Transport:recv(Socket, 74, 5000) of
         {ok, <<ClientPub:256?UINT, M1:160?UINT, _Rest/binary>>} ->
             lager:debug("Received auth proof (pub: ~p, m1: ~p)", [ClientPub, M1]),
-            SN = srp6:proof(S, ClientPub, <<"TEST">>),
-            lager:debug("Calculated session proof ~d", [SN#session.session_proof]),
+            {ok, SN} = srp6:proof(S, ClientPub, <<"TEST">>),
+            lager:debug("Calculated session proof ~p", [SN#session.session_proof]),
             ok = Transport:send(Socket, <<1, 0, (SN#session.session_proof):160?UINT,
-                                          0, 0:64, 0:16>>),
+                                          0:32>>),
             SN;
 %            ok = Transport:send(Socket, <<0, 0, ?ce_server_full>>);
         _ ->
--- src/srp6.erl
+++ src/srp6.erl
@@ -1,6 +1,6 @@
 -module(srp6).
 -export([challenge/1, proof/3]).
-
+-compile(export_all).
 -include("realmd_defines.hrl").
 
 
@@ -22,38 +22,71 @@ odd(<<_:8,X:8>>) ->          <<X>>;
 odd(<<X:8>>) ->              <<X>>;
 odd(<<_:8,X:8,Z/binary>>) -> <<X:8, (odd(Z))/binary>>.
 
-%% @spec binzip(binary(), binary()) -> binary().
-binzip(<<>>, <<>>) ->
-    <<>>;
-binzip(<<H1:8, T1/binary>>, <<H2:8, T2/binary>>) ->
-    <<(binzip(T1, T2))/binary, H1:8, H2:8>>.
+%% %% @spec binzip(binary(), binary()) -> binary().
+%% binzip(<<>>, <<>>) ->
+%%     <<>>;
+%% binzip(<<H1:8, T1/binary>>, <<H2:8, T2/binary>>) ->
+%%     <<(binzip(T1, T2))/binary, H1:8, H2:8>>.
+binzip(Bin1, Bin2) ->
+    Bin1L = binary_to_list(Bin1),
+    Bin2L = binary_to_list(Bin2),
+    Zipped = lists:flatten(lists:map(fun(X) -> tuple_to_list(X) end,
+                                     lists:zip(Bin1L, Bin2L))),
+    list_to_binary(Zipped).
 
 %% @spec challenge(binary()) -> #session{}
 challenge(Hash) ->
     Salt       = getBignum(),
     SaltedHash = sha_int(<<Salt:256?UINT, Hash/bytes>>),
-    Verifier   = crypto:mod_exp(?g, SaltedHash, 15),
+    Verifier   = crypto:mod_exp(?g, SaltedHash, ?safe_prime),
     Priv       = getBignum(),
     Gmod       = crypto:mod_exp(?g, Priv, ?safe_prime),
     Pub        = crypto:mod_exp(Verifier * 3 + Gmod, 1, ?safe_prime),
     #session{hash=Hash, salt=Salt, public=Pub, private=Priv, verifier=Verifier}.
 
-proof(S, ClientPub, AccountName) ->
-    U  = sha_int(<<ClientPub:256?UINT, (S#session.public):256?UINT>>),
-    S1 = crypto:mod_exp(S#session.verifier, U, ?safe_prime),
-    S2 = crypto:mod_exp(S1 * ClientPub, S#session.private, ?safe_prime),
-    T0 = <<S2:256?UINT>>,
-    T1 = <<(sha_int(even(T0))):160?UINTB>>,
-    T2 = <<(sha_int(odd(T0))):160?UINTB>>,
-    SK = binzip(T1, T2),
-    SP = sha_int(<<?safe_prime:256?UINT>>),
-    X  = sha_int(<<?g>>),
-    SX = SP bxor X,
-    AN = sha_int(AccountName),
-    CP = sha_int(<<SX:160?UINT, AN:160?UINT, (S#session.salt):256?UINT,
-                   ClientPub:256?UINT, (S#session.public):256?UINT, SK/binary>>),
-    SSP = sha_int(<<ClientPub:256?UINT, CP:160?UINT, SK/binary>>),
-    S#session{session_key = SK, client_proof = CP, session_proof = SSP}.
+%% @spec proof(integer(), integer(), binary()) -> integer().
+%% proof(S, ClientPub, _Acc) ->
+%%     U  = sha_int(<<ClientPub:256?UINT, (S#session.public):256?UINT>>),
+%%     T1 = crypto:compute_key(srp, ClientPub, {S#session.public, S#session.private},
+%%                             {host, [binary:encode_unsigned(S#session.verifier),
+%%                                     binary:encode_unsigned(?safe_prime),
+%%                                     '6', % SRP version
+%%                                     binary:encode_unsigned(U)]}),
+%%     T2 = crypto:compute_key(srp, ClientPub, {S#session.public, S#session.private},
+%%                             {host, [binary:encode_unsigned(S#session.verifier, little),
+%%                                     binary:encode_unsigned(?safe_prime, little),
+%%                                     '6', % SRP version
+%%                                     binary:encode_unsigned(U, little)]}),
+%%     SessionKey = sha_int(T1),
+%%     SessionKey2 = sha_int(T2),
+%%     lager:debug("~nT1 ~p~nT2 ~p~n", [SessionKey, SessionKey2]),
+%%     S#session{session_proof = SessionKey2}.
+
+%% proof(S, ClientPub, AccountName) ->
+%%     U  = sha_int(<<ClientPub:256?UINT, (S#session.public):256?UINT>>),
+%%     S1 = crypto:mod_exp(S#session.verifier, U, ?safe_prime),
+%%     S2 = crypto:mod_exp(S1 * ClientPub, S#session.private, ?safe_prime),
+%%     T0 = <<S2:256?UINT>>,
+%%     T1 = <<(sha_int(even(T0))):160?UINTB>>,
+%%     T2 = <<(sha_int(odd(T0))):160?UINTB>>,
+%%     SK = binzip(T1, T2),
+%%     40 = byte_size(SK), % assert this
+%%     SP = sha_int(<<?safe_prime:256?UINT>>),
+%%     X  = sha_int(<<?g>>),
+%%     SX = SP bxor X,
+%%     AN = sha_int(AccountName),
+%%     CP = sha_int(<<SX:160?UINT, AN:160?UINT, (S#session.salt):256?UINT,
+%%                    ClientPub:256?UINT, (S#session.public):256?UINT, SK/binary>>),
+%%     lager:debug("CP ~p~n", [CP]),
+%%     SSP = sha_int(<<ClientPub:256?UINT, CP:160?UINT, SK/binary>>),
+%%     %SSP = sha_int(<<CP:160?UINT, SK/binary>>),
+%%     {ok, S#session{session_key = SK, client_proof = ClientPub,
+%%                    session_proof = SSP}}.
+
+%% Implementation according to spec
+%% @spec proof(#session{}, integer(), binary()) -> #session{}.
+proof(SE, A, I) ->
+    fail.
 
 % randomly generated 32 byte number
 getBignum() ->
